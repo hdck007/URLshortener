4f5e8cf329994832b45e52989aca91d7c0f44d84
* <haridarshanofficial@gmail.com>
2020-08-26 09:23:21 +0530

    Some seccurity changes removed secret key

--- short/settings.py
+++ short/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9&e=2zpegy+scw-7%+y*4b-010adf&4lf!h5d&%q4nh$w$muaq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
